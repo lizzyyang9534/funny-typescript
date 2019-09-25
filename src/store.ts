906cb9436f73fe7a3dddac3e71872ec918646fe2
TITANSOFT\lizzy.yang71 <lizzy.yang@titansoft.com.sg>
2019-09-25 14:49:02 +0800

    add vuex

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,33 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
+
+const state: IState = {
+  isBark: false
+};
+
+export interface IState {
+  isBark: boolean;
+}
+
+export const actionName = {
+  bark: "bark"
+};
 
 export default new Vuex.Store({
-  state: {
-
-  },
+  state: state,
   mutations: {
-
+    [actionName.bark](state: IState, isBark: boolean) {
+      state.isBark = isBark;
+    }
   },
   actions: {
-
+    [actionName.bark](context) {
+      context.commit(actionName.bark, true);
+      setTimeout(() => {
+        context.commit(actionName.bark, false);
+      }, 1000);
+    }
   }
-})
+});
